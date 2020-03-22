4f1f6d931fedf7bf0ca866865cc39b8086999ef3
sagar-528 <gupta.sagar528@gmail.com>
2020-03-22 12:56:52 +0530

    ADDED: Exact Winning

--- snakeAndLadder.sh
+++ snakeAndLadder.sh
@@ -5,40 +5,58 @@
 
 #Constant
 POSITION=0
+WINNING_POSITION=100
 
 #Variable
 position=0
-ladderCount=1
-snakeCount=2
 
 echo "Snake And Ladder Game"
-player=$((RANDOM%6+1))			#Random Function for roll the die from 1 to 6.
-echo "Outcomes are" $player
-
+#Random Function for roll the die from 1 to 6.
 #Winning Condition
-while(($position!=100))
+while(($position!=$WINNING_POSITION))
 do
+	player=$((RANDOM%6+1))
+	echo "Outcomes are" $player
+
 	case $((RANDOM%3)) in
-	#No play the player stays in the same position
+		#No play the player stays in the same position
 		0)
 			echo "Current position is: "$position
-			echo "you stay in same position: "$position ;;
-	#Ladder
+			echo "you stay in same position: "$position
+			position=$position 
+			echo " " ;;
+
+		#Ladder
 		1)
-			((ladderCount++))
 			echo "Current position is: "$position
-			position=$((position+player))
-			echo "Player Ladder moves ahead: "$position ;;
-	#Snake
+			echo "You encountered a ladder"
+		
+			#Exact winning position
+			if (( $((position+player))>100 ))
+			then
+				position=$position
+				echo "Current position is: "$position
+			else
+				position=$((position+player))
+				echo "Player Ladder moves ahead: "$position 
+			fi
+				echo " "  ;;
+		
+		#Snake
 		2)
-			((snakeCount++))
+			echo "You encountered snake"
 			echo "Current position is: "$position
-			position=$((position-player))
+		
+			#Exact winning position.
 			if (($position<0))
 			then
 				echo "Player Snake moves behind: "$POSITION
+				position=$POSITION
 			else
+				position=$((position-player))
 				echo "Player moves behind: "$position
-			fi ;;
+				position=$position
+			fi 
+			echo " " ;;
 	esac
 done
